6279529b2a49918b97934f1d00111ca57cf1be56
Naive <sooo.naive@yandex.ru>
2022-03-27 16:09:56 +0300

    post

---
+++ POST/axios/js/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+
+    function req(e) {
+        e.preventDefault();
+        
+        let formData = new FormData(form);
+        formData.append("id", Math.random());
+
+        let obj = {};
+        formData.forEach((value, key) => {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+
+      axios.post("http://localhost:3000/people", obj);
+    }
+
+    form.addEventListener("submit", (e) => req(e), {"once": true});
+
+
+    async function getResource(url, data) {
+        const res = await fetch(`${url}`, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+        return await res.json();
+    }
+});
---
+++ POST/formData/js/script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+
+    function req(e) {
+        e.preventDefault();
+        
+        let formData = new FormData(form);
+        formData.append("id", Math.random());
+
+        let obj = {};
+        formData.forEach((value, key) => {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+
+    	const request = new XMLHttpRequest();
+        request.open("POST", "http://localhost:3000/people");
+        request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+        request.send(json);
+        request.addEventListener("load", function() {
+        	if (request.status == 200) {
+        		let data = JSON.parse(request.response);
+        		console.log(data);
+
+        	} else {
+        		console.error("Что-то поло не так");
+        	}
+        });
+    }
+
+    form.addEventListener("submit", (e) => req(e), {"once": true});
+
+});
