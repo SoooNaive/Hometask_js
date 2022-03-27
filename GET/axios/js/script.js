af76420cca32a2cef5e01bad36b1ac5c0ef56aa5
Naive <sooo.naive@yandex.ru>
2022-03-27 16:09:34 +0300

    get

---
+++ GET/axios/js/script.js
@@ -0,0 +1,45 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+    function req() {
+        getResource("http://localhost:3000/people")
+            .then(data => createCards(data.data))
+            .catch(err => console.error(err));
+        this.remove();
+    }
+
+    document.querySelector("button").addEventListener("click", req, {"once": true});
+
+     async function getResource(url) {
+        const res = await axios(`${url}`);
+        if (res.status !== 200) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+        return res;
+    }
+
+
+    function createCards(response) {
+        response.forEach(item => {
+                    let card = document.createElement('div');
+                    cord.classList.add('card');
+
+                    let icon;
+                    if (item.sex === "male") {
+                        icon = "icons/mars.png";
+                    } else {
+                        icon = "icons/female.png";
+                    }
+
+                    card.innerHTML = `
+                        <img src="${item.photo}" alt="photo">
+                        <div class="name">${item.name} ${item.surname}</div>
+                        <div class="sex">
+                            <img src=${icon} alt="male">
+                        </div>
+                        <div class="age">${item.age}</div>
+                    `;
+                    document.querySelector('.app').appendChild(card);
+                });
+    }
+});
---
+++ GET/fetch/js/script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+    function req() {
+        getResource("http://localhost:3000/people")
+            .then(data => createCards(data))
+            .catch(err => console.error(err));
+        this.remove();
+    }
+
+    document.querySelector("button").addEventListener("click", req, {"once": true});
+    
+    async function getResource(url) {
+        const res = await fetch(`${url}`);
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+        return await res.json();
+    }
+
+    function createCards(response) {
+        response.forEach(item => {
+                    let card = document.createElement('div');
+                    cord.classList.add('card');
+
+                    let icon;
+                    if (item.sex === "male") {
+                        icon = "icons/mars.png";
+                    } else {
+                        icon = "icons/female.png";
+                    }
+
+                    card.innerHTML = `
+                        <img src="${item.photo}" alt="photo">
+                        <div class="name">${item.name} ${item.surname}</div>
+                        <div class="sex">
+                            <img src=${icon} alt="male">
+                        </div>
+                        <div class="age">${item.age}</div>
+                    `;
+                    document.querySelector('.app').appendChild(card);
+                });
+    }
+});
